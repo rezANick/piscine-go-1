393109795bcea08ffe53184a17f2a09ac13a6174
SuperSidick <youssouf.sidick.ys@outlook.com>
2019-06-10 11:02:43 +0000

    My very first commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+HERO_ID=70
 var=$( curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID" '.[] | select( .id==$id) | .connections.relatives' | sed -e 's/" //g')
 
-echo "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
+echo "Damian Wayne (son), Dick Grayson (adopted son), Tim Drake (adopted son), Jason Todd (adopted son), Cassandra Cain (adopted ward)\nMartha Wayne (mother, deceased), Thomas Wayne (father, deceased), Alfred Pennyworth (former guardian), Roderick Kane (grandfather, deceased), Elizabeth Kane (grandmother, deceased), Nathan Kane (uncle, deceased), Simon Hurt (ancestor), Wayne Family"
