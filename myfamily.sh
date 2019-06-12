da8eaa3aa5035c60139bc0f824a3db424366229f
SuperSidick <youssouf.sidick.ys@outlook.com>
2019-06-12 16:59:25 +0000

    My very first commit

--- myfamily.sh
+++ myfamily.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-HERO_ID=70
-var=$( curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --argjson id "$HERO_ID" '.[] | select( .id==$id) | .connections.relatives' | sed -e 's/" //g')
 
-echo "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
+var=$( curl -s https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg HERO_ID "$HERO_ID" '.[] | select ( .id == ($HERO_ID|tonumber)) | .connections.relatives' | sed 's/"//g')
+
+echo $var
